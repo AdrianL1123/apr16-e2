48706a3299a9616e89fc723e76c9cfc13cab68b4
[AdrianL] <[adrianlee061123@gmail.com]>
2024-04-17 09:42:01 +0800

    apr17 com

--- visitor.js
+++ visitor.js
@@ -26,42 +26,70 @@ let reservations = [
 
 //*  Your routing, authentication, and controller code goes here
 app.get("/parks", (request, response) => {
-  response.status(200).json(parks);
+  response.status(200).send(parks);
 });
 
 app.get("/parks/:id", (request, response) => {
   const parksId = parseInt(request.params.id);
+  //* can also convert i.id to a string or req.params.id to number either of it can work
   const selected = parks.find((i) => i.id === parksId);
-  response.status(200).json(selected);
+  response.status(200).send(selected);
 });
 
 app.get("/visitors", (request, response) => {
-  response.status(200).json(visitors);
+  response.status(200).send(visitors);
 });
 
 app.get("/visitors/:id", (request, response) => {
   //   const visitorId = parseInt(request.params.id);
   const visitor = visitors.find((v) => v.id == request.params.id);
   if (visitor) {
-    const reservations = reservations.find((r) => r.id);
-    response.status(200).json({
-      ...visitors,
-      upcomingReservations: reservations.upcomingReservations || [],
-      pastReservations: reservations.pastReservations || [],
+    //* long method
+    // let newPastReservations = [];
+    // visitor.pastReservations.forEach( r_id => {
+    //   const reservation = reservations.find( r => r.id === r_id );
+    //   if ( reservation )
+    //     newPastReservations.push(reservation);
+    // })
+    // let newUpcomingReservations = [];
+    // visitor.upcomingReservations.forEach( r_id => {
+    //   const reservation = reservations.find( r => r.id === r_id );
+    //   if ( reservation )
+    //   newUpcomingReservations.push(reservation);
+    // })
+    // res.status(200).send({
+    //   ...visitor,
+    //   pastReservations: newPastReservations,
+    //   upcomingReservations: newUpcomingReservations
+    // });
+    response.status(200).send({
+      ...visitor,
+      pastReservations: visitor.pastReservations.map((r_id) => {
+        const reservation = reservations.find((r = r.id === r_id));
+        if (reservation) {
+          return reservation;
+        }
+      }),
+      upcomingReservations: visitor.upcomingReservations.map((r_id) => {
+        const reservation = reservations.find((r = r.id === r_id));
+        if (reservation) {
+          return reservation;
+        }
+      }),
     });
   } else {
-    response.status(404).json("visitor not found");
+    response.status(404).send("visitor not found");
   }
 });
 
 app.get("/reservations", (request, response) => {
-  response.status(200).json(reservations);
+  response.status(200).send(reservations);
 });
 
 app.get("/reservations/:id", (request, response) => {
   const reservationsId = parseInt(request.params.id);
   const selectedR = reservations.find((i) => i.id === reservationsId);
-  response.status(200).json(selectedR);
+  response.status(200).send(selectedR);
 });
 
 app.listen(8880, () => {
